3c7841a3b8ff7b62ba121f0d2f2aa62289363b6f
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-17 19:28:24 +0500

    ozagrish kiritim

---
+++ EmployeeApi/Services/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeApi.Dtos;
+using EmployeeApi.Entities;
+
+namespace EmployeeApi.Services
+{
+    public interface IEmployeeService
+    {
+        public ValueTask<bool> CreateAsync(EmployeeDto dto);
+
+        public ValueTask<bool> DeleteASync(long EmployeeId);
+
+        public ValueTask<IList<Employee>> GetAllAsync();
+    }
+}
