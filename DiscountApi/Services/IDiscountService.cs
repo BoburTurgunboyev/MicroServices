6cdde76fe5267ec4683859c7c2647fbb74c6d4f9
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-17 19:30:05 +0500

    ozagrish kiritim

---
+++ DiscountApi/Services/IDiscountService.cs
@@ -0,0 +1,15 @@
+﻿using DiscountApi.Dtos;
+using DiscountApi.Entities;
+
+namespace DiscountApi.Services
+{
+    public interface IDiscountService
+    {
+        public ValueTask<bool> CreateAsync(DiscountDto dto);
+
+        public ValueTask<bool> DeleteASync(long DiscountId);
+
+
+        public ValueTask<IList<Discount>> GetAllAsync();
+    }
+}
