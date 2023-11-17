1f3b6471d2009c43fd9ac47c1467d6b1770dcbf5
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-17 19:31:02 +0500

    ozagrish kiritim

---
+++ DiscountApi/Controllers/DiscountController.cs
@@ -0,0 +1,43 @@
+﻿using DiscountApi.Dtos;
+using DiscountApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiscountApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountService _discountService;
+
+        public DiscountController(IDiscountService discountService)
+        {
+            _discountService = discountService;
+        }
+
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateAsync(DiscountDto componyDto)
+        {
+            var res = await _discountService.CreateAsync(componyDto);
+            return Ok(res);
+        }
+
+        [HttpGet]
+
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            var res = await _discountService.GetAllAsync();
+            return Ok(res);
+        }
+
+        [HttpDelete]
+
+        public async ValueTask<IActionResult> DeleteAsync(long id)
+        {
+            var res = await _discountService.DeleteASync(id);
+            return Ok(res);
+        }
+    }
+}
