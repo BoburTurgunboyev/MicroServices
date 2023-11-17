65310a3a02fa7f8b96b023d5c204fbc8588aa1de
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-17 19:28:30 +0500

    ozagrish kiritim

---
+++ EmployeeApi/Services/EmployeeService.cs
@@ -0,0 +1,51 @@
+﻿using EmployeeApi.DataAccess;
+using EmployeeApi.Dtos;
+using EmployeeApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeApi.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+
+        private DbConent _dbConent;
+
+        public EmployeeService(DbConent dbConent)
+        {
+            _dbConent = dbConent;
+        }
+        public async ValueTask<bool> CreateAsync(EmployeeDto dto)
+        {
+            Employee employee = new Employee()
+            {
+               FirstName = dto.FirstName,
+               LastName = dto.LastName,
+               Password = dto.Password,
+
+
+            };
+
+            var result = await _dbConent.Employees.AddAsync(employee);
+            await _dbConent.SaveChangesAsync();
+            if (result is not null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async ValueTask<bool> DeleteASync(long EmployeeId)
+        {
+            var result = await _dbConent.Employees.FirstOrDefaultAsync(x => x.Id == EmployeeId);
+
+            var res = _dbConent.Employees.Remove(result);
+            await _dbConent.SaveChangesAsync();
+            return res != null;
+        }
+
+        public async ValueTask<IList<Employee>> GetAllAsync()
+        {
+            return await _dbConent.Employees.ToListAsync();
+        }
+    }
+}
