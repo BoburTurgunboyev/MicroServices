24736e8b9a3d4643d686063a59719e82995f9033
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-17 19:32:19 +0500

    ozagrish kiritim

---
+++ ComponyApi/DataAccess/DbConent.cs
@@ -0,0 +1,14 @@
+﻿using ComponyApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComponyApi.DataAccess
+{
+    public class DbConent:DbContext
+    {
+        public DbConent(DbContextOptions<DbConent> options) : base(options)
+        {
+        }
+
+        public DbSet<Compony> Componys { get; set; } 
+    }
+}
