9897e9140b05e1f32f28e8d575e815d1b14496b7
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-17 19:29:08 +0500

    ozagrish kiritim

---
+++ EmployeeApi/Entities/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeApi.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+    }
+}
