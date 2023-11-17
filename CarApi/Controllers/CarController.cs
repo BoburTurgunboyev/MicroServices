7884d2f83b1a0916a593c2cf1df732ea9fbce672
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-17 19:33:42 +0500

    ozagrish kiritim

---
+++ CarApi/Controllers/CarController.cs
@@ -0,0 +1,45 @@
+﻿using CarApi.Dtos;
+using CarApi.Entities;
+using CarApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace CarApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        private ICarService _carService;
+
+        public CarController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpPost]
+        public async  ValueTask <IActionResult> CreateAsync(CarDto carDto)
+        {
+            var res = await _carService.CreateAsync(carDto);
+            return Ok(res);
+        }
+
+        [HttpGet]
+
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            var res =  await _carService.GetAllAsync();
+            return Ok(res);
+        }
+
+        [HttpDelete]
+
+        public async ValueTask<IActionResult> DeleteAsync(long id)
+        {
+            var res = await _carService.DeleteASync(id);
+            return Ok(res);
+        }
+       
+    }
+}
