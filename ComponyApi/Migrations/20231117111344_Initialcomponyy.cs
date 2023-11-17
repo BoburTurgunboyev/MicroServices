d5bdeabe4f5a2fbebf14e8abe4ec02005580903b
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-17 19:32:02 +0500

    ozagrish kiritim

---
+++ ComponyApi/Migrations/20231117111344_Initialcomponyy.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComponyApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialcomponyy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Componys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Componys", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Componys");
+        }
+    }
+}
