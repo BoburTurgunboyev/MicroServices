f5195b8f637739b46209848adc9a06fe61ae208e
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-17 19:31:38 +0500

    ozagrish kiritim

---
+++ ComponyApi/Services/IComponyService.cs
@@ -0,0 +1,16 @@
+﻿using ComponyApi.Dtos;
+using ComponyApi.Entities;
+
+namespace ComponyApi.Services
+{
+    public interface IComponyService
+    {
+
+        public ValueTask<bool> CreateAsync(ComponyDto dto);
+
+        public ValueTask<bool> DeleteASync(long ComponyId);
+
+
+        public ValueTask<IList<Compony>> GetAllAsync();
+    }
+}
