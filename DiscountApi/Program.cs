355a7f3a7b8ad0c7c3bb6acfdd97ece7a0668372
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-17 19:29:46 +0500

    ozagrish kiritim

--- DiscountApi/Program.cs
+++ DiscountApi/Program.cs
@@ -1,7 +1,13 @@
+using DiscountApi.DataAccess;
+using DiscountApi.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IDiscountService, DiscountService>();
+builder.Services.AddDbContext<DbConent>(options
+    => options.UseSqlServer(builder.Configuration.GetConnectionString("Connected")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
