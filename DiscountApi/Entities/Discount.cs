a12badc71082e5b2f97d84cafb7367d24029f7f1
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-17 19:30:37 +0500

    ozagrish kiritim

---
+++ DiscountApi/Entities/Discount.cs
@@ -0,0 +1,10 @@
+﻿namespace DiscountApi.Entities
+{
+    public class Discount
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
