1c2361660c6ae732beb4a53379b64bbab8d840eb
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-17 19:30:49 +0500

    ozagrish kiritim

---
+++ DiscountApi/DataAccess/DbConent.cs
@@ -0,0 +1,16 @@
+﻿using DiscountApi.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DiscountApi.DataAccess
+{
+    public class DbConent:DbContext
+    {
+        public DbConent(DbContextOptions<DbConent> options) : base(options)
+        {
+        }
+
+        public DbSet<Discount> Discounts { get; set; }
+    }
+}
+    
