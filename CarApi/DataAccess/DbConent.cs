888dabdd7da66921e92694e3a8eb518a3cad3ec2
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-17 19:33:36 +0500

    ozagrish kiritim

---
+++ CarApi/DataAccess/DbConent.cs
@@ -0,0 +1,17 @@
+﻿using CarApi.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CarApi.DataAccess
+{
+    public class DbConent:DbContext
+    {
+        public DbConent(DbContextOptions<DbConent> options) : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }    
+    }
+
+    
+}
