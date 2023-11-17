f95ff076ed304c025027e959fd3341253da5cfa2
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-17 19:32:07 +0500

    ozagrish kiritim

---
+++ ComponyApi/Entities/Compony.cs
@@ -0,0 +1,10 @@
+﻿namespace ComponyApi.Entities
+{
+    public class Compony
+    {
+        public int Id { get; set; }
+        public string CompName { get; set; }
+        public string Location { get; set; }
+
+    }
+}
