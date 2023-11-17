42b24cf92f2c3185a35e9400411b0f4216f329a4
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-17 19:29:15 +0500

    ozagrish kiritim

---
+++ EmployeeApi/Dtos/EmployeeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeApi.Dtos
+{
+    public class EmployeeDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+    }
+}
