845682060da9623e5f3352d50406c49314f0f64a
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-17 19:33:26 +0500

    ozagrish kiritim

---
+++ CarApi/Entities/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace CarApi.Entities
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string CarName { get; set; }
+        public string Brent { get; set; }
+    }
+}
