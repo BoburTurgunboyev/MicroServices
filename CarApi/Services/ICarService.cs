fc9b2dbe3786b21b7f42148504660d9f4f7d128a
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-17 19:32:56 +0500

    ozagrish kiritim

---
+++ CarApi/Services/ICarService.cs
@@ -0,0 +1,18 @@
+﻿using CarApi.Dtos;
+using CarApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarApi.Services
+{
+    public interface ICarService
+    {
+        public ValueTask<bool> CreateAsync(CarDto dto);
+       
+        public ValueTask<bool> DeleteASync(long CarId);
+     
+
+        public ValueTask<IList<Car>> GetAllAsync();
+
+       
+    }
+}
