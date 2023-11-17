9cc0f65e375ae54ee730e74a628cc6725d16c805
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-17 19:29:22 +0500

    ozagrish kiritim

---
+++ EmployeeApi/DataAccess/DbConent.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeApi.DataAccess
+{
+    public class DbConent:DbContext
+    {
+        public DbConent(DbContextOptions<DbConent> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
